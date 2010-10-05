4b2f313f91c5d1aa67c0fea8f623d2296a0ccd5d
nixon <nixon@shun.org>
2010-10-05 11:43:26 -0500

    added "launch.py --routes" option
    http://groups.google.com/group/python-tornado/msg/0fe7b4ebc4f4da49

--- launch.py
+++ launch.py
@@ -7,6 +7,9 @@ import sys
 import tornapp  # our application
 from settings import torn_settings as settings
 
+from optparse import OptionParser
+from tornapp.views.viewlib import route
+
 def start_instance(settings):
     http_server = tornado.httpserver.HTTPServer(
         tornapp.setup_app(settings)
@@ -18,6 +21,21 @@ def start_instance(settings):
 
 
 if __name__ == "__main__":
-    try: settings['httpd_port'] = int(sys.argv[1])
-    except: pass
+    parser = OptionParser()
+    parser.add_option("-r", "--routes", action="store_true",
+            help="print list of routes")
+    parser.add_option("-p", "--httpd-port", help="specify httpd port")
+    (options, args) = parser.parse_args()
+
+    if options.routes:
+        for r,c in  route.get_routes():
+            print "%s => %s" % (r,c.__name__)
+        sys.exit(0)
+    elif options.httpd_port:
+        try: settings['httpd_port'] = int(options.httpd_port)
+        except: pass
+    elif args:
+        try: settings['httpd_port'] = int(args[0])
+        except: pass
+
     start_instance(settings)
